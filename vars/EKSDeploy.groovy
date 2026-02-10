34000ff0d2dcd6a15732526419bd136e3d720cfa
Rohith1845 <krishnamsettyrohith@gmail.com>
2026-02-10 12:54:49 +0530

    success

---
+++ vars/EKSDeploy.groovy
@@ -0,0 +1,57 @@
+    def call (Map configMap){
+        pipeline {
+    // These are pre-build sections
+    agent {
+        node {
+            label 'Agent-1'
+        }
+    }
+    environment {
+        COURSE = "Jenkins"
+        appVersion = configMap.get("appVersion")
+        ACC_ID = "376798132299"
+        project = configMap.get("project")
+        component = configMap.get("component")
+        deploy_to = configMap.get("deploy_to")
+        REGION = "us-east-1"
+    }
+    options {
+        timeout(time: 10, unit: 'MINUTES') 
+        disableConcurrentBuilds()
+    }
+    // This is build section
+    
+    stages { 
+        stage('Deploy') {
+            steps {
+                script{
+                    withAWS(region:'us-east-1',credentials:'aws-creds'){
+                        sh """
+                            aws eks -update-kubeconfig --region ${REGION} --name ${project}-${deploy_to}
+                            kubectl get nodes
+                            echo "${deploy_to}, ${appVersion}"
+                        """
+                    }
+                }
+            }
+        }   
+        
+
+    }
+    post{
+        always{
+            echo 'I will always say Hello again!'
+            cleanWs()
+        }
+        success {
+            echo 'I will run if success'
+        }
+        failure {
+            echo 'I will run if failure'
+        }
+        aborted {
+            echo 'pipeline is aborted'
+        }
+    }
+}
+}
