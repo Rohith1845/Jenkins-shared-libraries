ba560db736bff3748321382dedc6d20ba47b3654
Rohith1845 <krishnamsettyrohith@gmail.com>
2026-02-14 14:30:30 +0530

    success

--- vars/pythonEKSPipeline.groovy
+++ vars/pythonEKSPipeline.groovy
@@ -1,5 +1,5 @@
 def call(Map configMap){
-        def appVersion = ""
+        
         pipeline {
         // These are pre-build sections
         agent {
@@ -9,7 +9,7 @@ def call(Map configMap){
         }
         environment {
             COURSE = "Jenkins"
-            
+            def appVersion = ""
             ACC_ID = "131315333865"
             PROJECT = configMap.get("project")
             COMPONENT = configMap.get("component")
